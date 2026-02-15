6f53dd34a5e68c4f1b478d9f8be406ab4325ce79
Sean Cairns <sean.cairnsst@gmail.com>
2026-02-15 12:14:12 +0000

    Clean up

--- flake.nix
+++ flake.nix
@@ -2,14 +2,6 @@
   outputs =
     { ... }:
     {
-      templates.python313 = {
-        path = ./python313;
-        description = "Python 3.13 development template";
-      };
-      templates.python314 = {
-        path = ./python314;
-        description = "Python 3.14 development template";
-      };
       templates = {
         python313 = {
           path = ./templates/python313;
