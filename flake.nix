ab30e47d96b347caedde4425bc9de7a0f8fc0b31
Sean <101846471+Cairnstew@users.noreply.github.com>
2026-02-15 11:51:33 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,13 @@
   outputs =
     { ... }:
     {
-      templates.python313.path = ./python313;
-      templates.python314.path = ./python314;
+      templates.python313 = {
+        path = ./python313;
+        description = "Python 3.13 development template";
+      };
+      templates.python314 = {
+        path = ./python314;
+        description = "Python 3.14 development template";
+      };
     };
 }
