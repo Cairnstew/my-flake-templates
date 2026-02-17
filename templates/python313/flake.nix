d1cd5c0f2630d3e4bf4489d1b5f4ee4ad1b2fdeb
Sean Cairns <sean.cairnsst@gmail.com>
2026-02-17 11:26:23 +0000

    Update python flakes

--- templates/python313/flake.nix
+++ templates/python313/flake.nix
@@ -69,7 +69,7 @@
         let
           pkgs = nixpkgs.legacyPackages.${system};
           pythonSet = pythonSets.${system}.overrideScope editableOverlay;
-          virtualenv = pythonSet.mkVirtualEnv "hello-world-dev-env" workspace.deps.all;
+          virtualenv = pythonSet.mkVirtualEnv "dev-env" workspace.deps.all;
         in
         {
           default = pkgs.mkShell {
@@ -91,7 +91,7 @@
       );
 
       packages = forAllSystems (system: {
-        default = pythonSets.${system}.mkVirtualEnv "hello-world-env" workspace.deps.default;
+        default = pythonSets.${system}.mkVirtualEnv "env" workspace.deps.default;
       });
     };
 }
