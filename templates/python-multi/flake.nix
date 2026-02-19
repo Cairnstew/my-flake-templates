d260b4aa0be8f2009a94825d5b5b46095a296c03
Cairnstew <sean.cairnsst@gmail.com>
2026-02-19 15:38:55 -0600

    Update multi python flake.nix

--- templates/python-multi/flake.nix
+++ templates/python-multi/flake.nix
@@ -23,72 +23,67 @@
     };
   };
 
-  outputs =
-    {
-      nixpkgs,
-      pyproject-nix,
-      uv2nix,
-      pyproject-build-systems,
-      ...
-    }:
-    let
-    pythonVersions = [
-      "python310"
-      "python311"
-      "python312"
-    ];
-      inherit (nixpkgs) lib;
+  outputs = { self, nixpkgs, pyproject-nix, uv2nix, pyproject-build-systems, ... }:
+  let
+    # First python version is default
+    pythonVersions = [ "python312" "python311" "python313" ];
+    inherit (nixpkgs) lib;
 
-      format = nixpkgs.formats.yml {};
+    forAllSystems = lib.genAttrs lib.systems.flakeExposed;
 
-      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+    overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
+    editableOverlay = workspace.mkEditablePyprojectOverlay { root = "$REPO_ROOT"; };
 
-      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+    pythonSets = forAllSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in lib.genAttrs pythonVersions (pyName:
+        (pkgs.callPackage pyproject-nix.build.packages { python = pkgs.${pyName}; })
+          .overrideScope (lib.composeManyExtensions [
+            pyproject-build-systems.overlays.wheel
+            overlay
+          ])
+      )
+    );
 
-      overlay = workspace.mkPyprojectOverlay {
-        sourcePreference = "wheel";
-      };
-
-      editableOverlay = workspace.mkEditablePyprojectOverlay {
-        root = "$REPO_ROOT";
-      };
-
-      pythonSets = forAllSystems (system:
+    distPackages = forAllSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in lib.genAttrs pythonVersions (pyName:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        lib.genAttrs pythonVersions (pyName:
+          pythonSet = pythonSets.${system}.${pyName};
+          python = pkgs.${pyName};
+        in pkgs.stdenv.mkDerivation {
+          name = "dist-${pyName}";
+          src = ./.;
+          nativeBuildInputs = [
+            pkgs.uv
+            python
+          ];
+          buildPhase = ''
+            export HOME=$TMPDIR
+            export UV_NO_SYNC=1
+            export UV_PYTHON=${python.interpreter}
+            export UV_PYTHON_DOWNLOADS=never
+            uv build --out-dir dist
+          '';
+          installPhase = ''
+            mkdir -p $out/dist
+            cp dist/* $out/dist/
+          '';
+        }
+      )
+    );
+  in
+  {
+    devShells = forAllSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        shells = lib.genAttrs pythonVersions (pyName:
           let
-            python = pkgs.${pyName};
-          in
-          (pkgs.callPackage pyproject-nix.build.packages {
-            inherit python;
-          }).overrideScope
-            (lib.composeManyExtensions [
-              pyproject-build-systems.overlays.wheel
-              overlay
-            ])
-        )
-      );
-    in
-    {
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        lib.genAttrs pythonVersions (pyName:
-          let
-            pythonSet =
-              pythonSets.${system}.${pyName}.overrideScope editableOverlay;
-
-            virtualenv =
-              pythonSet.mkVirtualEnv "dev-env-${pyName}" workspace.deps.all;
-          in
-          pkgs.mkShell {
-            packages = [
-              virtualenv
-              pkgs.uv
-            ];
+            pythonSet = pythonSets.${system}.${pyName}.overrideScope editableOverlay;
+            virtualenv = pythonSet.mkVirtualEnv "dev-env-${pyName}" workspace.deps.all;
+          in pkgs.mkShell {
+            packages = [ virtualenv pkgs.uv ];
             env = {
               UV_NO_SYNC = "1";
               UV_PYTHON = pythonSet.python.interpreter;
@@ -99,15 +94,31 @@
               export REPO_ROOT=$(git rev-parse --show-toplevel)
             '';
           }
-        )
-      );
+        );
+        defaultShell = shells.${lib.head pythonVersions};
+      in shells // {
+        default = pkgs.mkShell {
+          inputsFrom = [ defaultShell ];
+          packages = [ pkgs.act ];
+          shellHook = ''
+            ${defaultShell.shellHook}
+            export DOCKER_HOST=$(docker context inspect --format '{{.Endpoints.docker.Host}}' 2>/dev/null || echo "unix:///run/user/$UID/docker.sock")
+          '';
+        };
+      }
+    );
 
-      packages = forAllSystems (system:
-        lib.genAttrs pythonVersions (pyName: {
-          default =
-            pythonSets.${system}.${pyName}
-              .mkVirtualEnv "env-${pyName}" workspace.deps.default;
-        })
-      );
-    };
+    packages = lib.recursiveUpdate
+      (forAllSystems (system:
+        lib.genAttrs pythonVersions (pyName:
+          pythonSets.${system}.${pyName}
+            .mkVirtualEnv "env-${pyName}" workspace.deps.default
+        )
+      ))
+      (forAllSystems (system:
+        lib.mapAttrs' (pyName: drv:
+          lib.nameValuePair "dist-${pyName}" drv
+        ) distPackages.${system}
+      ));
+  };
 }
